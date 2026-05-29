71f9736736fec23aa733429257e5b7b531de454f
Nihad <nihad.nemetli@gmail.com>
2026-05-29 23:30:49 +0400

    fix: errors fixed in task entity

--- src/main/java/com/tracker/tasktracker/entity/Task.java
+++ src/main/java/com/tracker/tasktracker/entity/Task.java
@@ -25,10 +25,10 @@ public class Task {
     public Long getId() {return id;}
 
     public String getDescription() {return description;}
-    public void setDescription(String description) {this.description = description}
+    public void setDescription(String description) {this.description = description;}
 
     public boolean isCompleted() {return completed;}
-    public void setCompleted(boolean completed) {this.completed = completed}
+    public void setCompleted(boolean completed) {this.completed = completed;}
 
     @Override
     public String toString() {
